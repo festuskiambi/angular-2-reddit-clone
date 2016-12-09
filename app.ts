06db6b7972cfb1be279f43e926a1f12e3a842d05
Festus kiambi <kiamfestoh@gmail.com>
2016-12-09 13:02:58 +0300

    addarticle function

--- app.ts
+++ app.ts
@@ -10,19 +10,29 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 @Component({
 selector: 'reddit',
 template:`
-<form class ="ui large form segment">
-<h3 class ="ui header">Add a Link </h3>
-
-<div class ="field">
-<label for "title"> Title:</label>
-<input name ="title"
+<form class="ui large form segment">
+<h3 class="ui header">Add a Link</h3>
+<div class="field">
+<label for="title">Title:</label>
+<input name="title"#newtitle >
 </div>
-
-
+<div class="field">
+<label for="link">Link:</label>
+<input name="link" #newlink>
+</div>
+<button (click)="addArticle(newtitle, newlink)"
+class="ui positive right floated button">
+Submit link
+</button>
 </form>
 `
 })
-class RedditApp { }
+class RedditApp {
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+  console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+  return false;
+}
+}
 
 
 
